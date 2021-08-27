2b1cc7f1f1a84d31cb4aa3182042deccc060d061
Muhammed <mferoglu97@gmail.com>
2021-08-27 09:52:24 +0300

    SwipeButtons added to AllAccountsFragment

--- app/src/main/java/com/example/intertech_account/view/main_page/fragment/account/AllAccountsFragment.kt
+++ app/src/main/java/com/example/intertech_account/view/main_page/fragment/account/AllAccountsFragment.kt
@@ -1,18 +1,25 @@
 package com.example.intertech_account.view.main_page.fragment.account
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.intertech_account.databinding.FragmentAllAccountsBinding
 import com.example.intertech_account.model.api_model.get_account.GetAccountModel
 import com.example.intertech_account.resources.common_variables.Constant
 import com.example.intertech_account.view.main_page.fragment.account.adapter.AllAccountsAdapter
+import com.example.intertech_account.view.main_page.fragment.account.adapter.Swipe
+import com.example.intertech_account.view.main_page.fragment.account.adapter.SwipeButton
+import com.example.intertech_account.view.main_page.fragment.account.adapter.SwipeButtonClickListener
 import com.example.intertech_account.view.main_page.fragment.main_page.MainPageFragmentDirections
 import com.example.intertech_account.view_model.GetAccountViewModel
 
@@ -40,6 +47,47 @@ class AllAccountsFragment : Fragment() {
                 adapter_!!.addAccount(getAccountModel.getAccountData.getAccountList)
 
         })
+        val swipe=object: Swipe(adapter,requireContext(),binding.allAccounts,200){
+            override fun instantiateSwipeButton(
+                viewHolder: RecyclerView.ViewHolder,
+                buffer: MutableList<SwipeButton>
+            ) {
+
+
+
+                buffer.add(
+                    SwipeButton(requireActivity(),
+                    "Deneme",
+                    30,
+                    0,
+                    Color.parseColor("#FF3C30"),
+                    object: SwipeButtonClickListener {
+                        override fun onClick(pos: Int) {
+                            Log.d("Info :","Deneme basıldı")
+                            print("asdasd")
+                            Toast.makeText(requireActivity(),"Delete ID"+pos, Toast.LENGTH_SHORT).show()
+                        }
+
+                    })
+                )
+
+                buffer.add(
+                    SwipeButton(requireActivity(),
+                    "Update",
+                    30,
+                    0,
+                    Color.parseColor("#FF9502"),
+                    object: SwipeButtonClickListener {
+                        override fun onClick(pos: Int) {
+                            Toast.makeText(requireActivity(),"Delete ID"+pos, Toast.LENGTH_SHORT).show()
+                        }
+
+                    })
+                )
+
+
+            }
+        }
         
         return binding.root
 
---
+++ app/src/main/java/com/example/intertech_account/view/main_page/fragment/account/adapter/SwipeButtonClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.intertech_account.view.main_page.fragment.account.adapter
+
+interface SwipeButtonClickListener {
+    fun onClick(pos:Int)
+}
