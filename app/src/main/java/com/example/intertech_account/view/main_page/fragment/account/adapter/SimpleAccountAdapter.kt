cb03369dc300a4271a44c73590dbb92287868d69
DogukanGun <dogukangundogan5@gmail.com>
2021-09-03 19:17:58 +0300

    Warningler duzeltildi

--- app/src/main/java/com/example/intertech_account/view/main_page/fragment/account/adapter/SimpleAccountAdapter.kt
+++ app/src/main/java/com/example/intertech_account/view/main_page/fragment/account/adapter/SimpleAccountAdapter.kt
@@ -1,32 +1,22 @@
 package com.example.intertech_account.view.main_page.fragment.account.adapter
 
 import android.annotation.SuppressLint
-import android.os.Build
-import android.view.LayoutInflater
+ import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
-import androidx.recyclerview.widget.RecyclerView
+ import androidx.recyclerview.widget.RecyclerView
 import com.example.intertech_account.databinding.HomeScreenTransactionRowBinding
-import com.example.intertech_account.databinding.SimpleAccountRecyclerviewRowBinding
-import com.example.intertech_account.model.api_model.get_account.GetAccountList
-import com.example.intertech_account.model.api_model.get_account_transaction_list.GetAccountTransactionList
+ import com.example.intertech_account.model.api_model.get_account_transaction_list.GetAccountTransactionList
 import com.example.intertech_account.model.api_model.status.SimpleAccountListState
 import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import java.util.*
+ import java.util.*
 import kotlin.collections.ArrayList
 
-class SimpleAccountAdapter() :RecyclerView.Adapter<SimpleAccountAdapter.SimpleAccountHolder>()  {
+class SimpleAccountAdapter :RecyclerView.Adapter<SimpleAccountAdapter.SimpleAccountHolder>()  {
         private  var transactions:Array<GetAccountTransactionList> = emptyArray()
         private var transactionArrayList:ArrayList<GetAccountTransactionList> = arrayListOf()
         var status:SimpleAccountListState = SimpleAccountListState.NO_FILTER
 
-        class SimpleAccountHolder(val binding: HomeScreenTransactionRowBinding): RecyclerView.ViewHolder(binding.root){
-
-        }
+        class SimpleAccountHolder(val binding: HomeScreenTransactionRowBinding): RecyclerView.ViewHolder(binding.root)
 
 
         // Ana sayfadaki transactionların response geldikten sonra eklenmesi
@@ -39,15 +29,14 @@ class SimpleAccountAdapter() :RecyclerView.Adapter<SimpleAccountAdapter.SimpleAc
         }
 
         @SuppressLint("SimpleDateFormat")
-        private fun filterArrayList(array:ArrayList<GetAccountTransactionList>, dayAmounts:Int):ArrayList<GetAccountTransactionList>{
+        private fun filterArrayList(dayAmounts:Int):ArrayList<GetAccountTransactionList>{
             return transactionArrayList.filter {
 
                 val date = SimpleDateFormat("dd-MM-yyyy").parse(it.time)!!
                 val calendar = Calendar.getInstance()
                 calendar.add(Calendar.DAY_OF_YEAR, dayAmounts)
 //                        val currentDate = SimpleDateFormat("dd-MM-yyyy").parse((Calendar.DAY_OF_MONTH.toString()+"-"+Calendar.MONTH.toString()+"-"+Calendar.YEAR.toString()))
-                var dateValue=date.after(calendar.time)
-                dateValue
+                date.after(calendar.time)
             } as ArrayList<GetAccountTransactionList>
         }
         @SuppressLint("SimpleDateFormat", "NotifyDataSetChanged")
@@ -56,19 +45,19 @@ class SimpleAccountAdapter() :RecyclerView.Adapter<SimpleAccountAdapter.SimpleAc
             transactionArrayList.addAll(transactions)
             when(status){
                 SimpleAccountListState.LAST_ONE_WEEK->{
-                    transactionArrayList=filterArrayList(transactionArrayList,SimpleAccountListState.LAST_ONE_WEEK.day)
+                    transactionArrayList=filterArrayList(SimpleAccountListState.LAST_ONE_WEEK.day)
                 }
                 SimpleAccountListState.LAST_ONE_MONTH->{
-                    transactionArrayList=filterArrayList(transactionArrayList,SimpleAccountListState.LAST_ONE_MONTH.day)
+                    transactionArrayList=filterArrayList(SimpleAccountListState.LAST_ONE_MONTH.day)
                 }
                 SimpleAccountListState.LAST_THREE_MONTHS->{
-                    transactionArrayList=filterArrayList(transactionArrayList,SimpleAccountListState.LAST_THREE_MONTHS.day)
+                    transactionArrayList=filterArrayList(SimpleAccountListState.LAST_THREE_MONTHS.day)
                 }
                 SimpleAccountListState.LAST_SIX_MONTHS->{
-                    transactionArrayList=filterArrayList(transactionArrayList,SimpleAccountListState.LAST_SIX_MONTHS.day)
+                    transactionArrayList=filterArrayList(SimpleAccountListState.LAST_SIX_MONTHS.day)
                  }
                 SimpleAccountListState.LAST_ONE_YEAR->{
-                    transactionArrayList=filterArrayList(transactionArrayList,SimpleAccountListState.LAST_ONE_YEAR.day)
+                    transactionArrayList=filterArrayList(SimpleAccountListState.LAST_ONE_YEAR.day)
                  }
                 SimpleAccountListState.DETAIL->{
 //                    transactionArrayList=filterArrayList(transactionArrayList,SimpleAccountListState.DETAIL.day)
@@ -108,13 +97,13 @@ class SimpleAccountAdapter() :RecyclerView.Adapter<SimpleAccountAdapter.SimpleAc
             return transactionArrayList.size
         }
 
-    private fun createDummyTransactionList(x: Double, d: String): GetAccountTransactionList {
-        var x = GetAccountTransactionList("test",d,"test","test",x,122.2,
-            "t","t","t","t","05-05-2010","t","t",
-            233.3,"t"
-        )
-        return x
-    }
+//    private fun createDummyTransactionList(x: Double, d: String): GetAccountTransactionList {
+//        var x = GetAccountTransactionList("test",d,"test","test",x,122.2,
+//            "t","t","t","t","05-05-2010","t","t",
+//            233.3,"t"
+//        )
+//        return x
+//    }
 
     private fun lastTenTransactions(item:ArrayList<GetAccountTransactionList>):ArrayList<GetAccountTransactionList>{
         val allTransactionsArrayList: ArrayList<GetAccountTransactionList> = arrayListOf()
@@ -134,9 +123,10 @@ class SimpleAccountAdapter() :RecyclerView.Adapter<SimpleAccountAdapter.SimpleAc
 
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun sortTransactionsDefault(){
        transactionArrayList.clear()
-        transactionArrayList.addAll(lastTenTransactions(transactions.toCollection(ArrayList<GetAccountTransactionList>())))
+        transactionArrayList.addAll(lastTenTransactions(transactions.toCollection(ArrayList())))
         notifyDataSetChanged()
     }
 
